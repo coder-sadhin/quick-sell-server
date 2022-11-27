8ad4009a9125fe372b883eb168f594713ed59299
coder-sadhin <codersadhin@gmail.com>
2022-11-27 21:14:29 +0600

    hide sr

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-const stripe = require("stripe")('sk_test_51M6TOhLZ8s0yewmCKIERlWDqgmuV0dUPMcqr6t68lquLbV9ES0l7wH2zsYyXgZUjwvvhxFeUujmMHDWRGVOZnxSM00E1Hd7kmq');
+const stripe = require("stripe")(process.env.SECRET_KEY);
 
 
 app.get('/', async (req, res) => {
